66ced5115dd5b297a79416450b77489fba03b9f2
VitorCarrijoGoncalves <vitorcg002@gmail.com>
2019-04-17 22:16:01 -0300

    Spark

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import static spark.Spark.*; 
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		get("/hello", (req, res) -> "Hello World");
+
+	}
+
+}
